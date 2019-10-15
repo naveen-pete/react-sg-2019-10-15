e89201d56605c25c8b7bf2cb719db89150dfba3f
naveen-pete <naveen.shenoy.blr@gmail.com>
2019-10-15 15:59:02 +0530

    commit es6 sample code into the repo

---
+++ apps/day-1/blogger-app/src/components/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+// function statement - ES5
+// function App() {
+//   return <h1>Blogger App</h1>;
+// }
+
+// function expression - ES5
+// const App = function () {
+//   return <h1>Blogger App</h1>;
+// }
+
+// ES6 arrow function
+// const App = () => {
+//   return <h1>Blogger App</h1>;
+// }
+
+// ES6 arrow function - shorter syntax
+const App = () => <h1>Blogger App</h1>;
+
+export default App;
